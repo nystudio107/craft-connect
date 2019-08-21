be81bfc8ec91e64c3e2dd01edd704db26d36196b
Andrew Welch <andrew@nystudio107.com>
2019-08-20 23:02:14 -0400

    Add VuePress docs
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Connect Documentation',
+    description: 'Documentation for the Connect plugin',
+    base: '/docs/connect/',
+    dest: '../../../sites/nystudio107/web/docs/connect',
+    themeConfig: {
+        sidebar: [
+            ['/', 'Connect plugin for Craft CMS 3.x'],
+        ]
+    }
+};
